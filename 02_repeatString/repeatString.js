c92010b45a6685e157fa37908be0b2f3967ac01a
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-05 18:29:41 -0400

    Completed all test cases for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(input, repeat) {
+    let output = "";
+    if (repeat < 0) {
+        return 'ERROR';
+    }
+    
+    for (let i = 0; i < repeat; i++) {
+        output += input;
+    }
+
+    return output;
 
 };
 
