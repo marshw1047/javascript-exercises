c756e633fa98eae8b366249c35c3865829e51f57
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-19 22:50:27 -0400

    Completed exercise 11 with lessons learned using .map and for..of vs for..in

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const titleArray = [];
 
+    for (let book of books) {
+        titleArray.push(book.title);
+    }
+
+    return titleArray;
 };
 
+//first thing is input/output
+//  so input is an array of objects with properties/keys, output is an array
+//  iterate through input array using for ... in
+//  instantiate the new array
+//  can add by using array.push(books.title)
 // Do not edit below this line
+
+// LESSONS LEARNED DIFF BETWEEN FOR..IN and FOR..OF
+// ALSO JUST USE .map()
+// return books.map((book) => book.title);
 module.exports = getTheTitles;
