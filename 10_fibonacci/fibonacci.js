54e9e9cc97f15b88e17efdbb98b0badbdb8b6874
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-19 22:35:48 -0400

    Added notes/comments on how to do with array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,6 @@
 const fibonacci = function(nth) {
     nth = Number(nth);
-    
+
     if (nth < 0) {
         return "OOPS";
     }
@@ -24,6 +24,13 @@ const fibonacci = function(nth) {
 
 
 };
+
+//NOTE: Doing with array.
+//      const fib = [0, 1];
+//      for (let i = 2; i <= count; i++) {
+//          fib[i] = fib[i - 1] + fib[i - 2];
+//      }
+//      return fib[count];
 
 // Do not edit below this line
 module.exports = fibonacci;
