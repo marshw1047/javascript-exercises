a0d6131289452861fcfdac79107766e44c404a3a
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-19 22:14:31 -0400

    Completed lesson 09. learned to use split,filter,join, and reverse

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    function isAlphanumeric(char) {
+        return /[a-z0-9]/.test(char);
+    }
+    const filteredString = string
+        .toLowerCase()
+        .split('')
+        .filter(isAlphanumeric)
+        .join('');
+
+    const reverseString = filteredString.split('').reverse().join('');
+
+    return filteredString === reverseString;
 };
 
 // Do not edit below this line
