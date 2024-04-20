6ca9ce3412f3f92900ff79f76e63d9bf80a4567c
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-19 21:55:41 -0400

    Completed 08_calculators

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+
+	for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let sum = 1;
 
+	for (let i = 0; i < array.length; i++) {
+    sum *= array[i];
+  }
+
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) {
+    return 1;
+  }
+  else return factorial(x -1) * x;
 };
 
 // Do not edit below this line
