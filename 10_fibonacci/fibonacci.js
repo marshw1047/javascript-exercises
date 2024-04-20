cb19d819e36607cbaa97ae858640a186056b4f71
Marshall <42623637+marshalljwu@users.noreply.github.com>
2024-04-19 22:31:20 -0400

    Completed exercise 10. Used switch cases and for loop. Base case for first 2 num then updated til reached nth number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    nth = Number(nth);
+    
+    if (nth < 0) {
+        return "OOPS";
+    }
+
+    switch (nth) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        default:
+            let nthSum = 0;
+            let a = 0;
+            let b = 1;
+            for (i = 2; i <= nth; i++) {
+                nthSum = a + b;
+                a = b;
+                b = nthSum;
+            }
+            return nthSum;
+    }
+
 
 };
 
